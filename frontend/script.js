54288088c77d62307e9fcf19eac72a8878c4d7b4
YashShah138 <89223735+YashShah138@users.noreply.github.com>
2023-05-24 10:26:14 -0700

    fix

--- frontend/script.js
+++ frontend/script.js
@@ -93,15 +93,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     // Send a request to the backend for a response
-    const response = await fetch('https://codex-im0y.onrender.com/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: data.get('prompt')
-        })
-    })
+    const response = /* connect to backend */
 
     clearInterval(loadInterval)
     messageDiv.innerHTML = " "
